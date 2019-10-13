01f94d5923f20ec9fb7d4a8d4f9e97b163ff750e
youngsouk <young34844@naver.com>
2019-10-13 04:07:07 +0000

    remove sigint

--- ex.py
+++ ex.py
@@ -204,12 +204,7 @@ def find_p_rdi_r():
 
 	return buffer
 
-def signal_handler(sig, frame):
-	print 'SIGINT is detected!'
-	raise SystemExit
-
 if __name__ == '__main__':
-	signal.signal(signal.SIGINT, signal_handler)
 	r = re.compile(r'[0-9]+.+\n') # function content line
 	r2 = re.compile(r'[^a-zA-Z\s]+') # function name
 	r3 = re.compile(r'[0-9]+') # function idx
